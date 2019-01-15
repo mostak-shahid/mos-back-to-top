300fe4cc1327d0c4cb3c29fad710d2f341b515c3
Md. Mostak Shahid <mostak.shaid@gmail.com>
2019-01-15 13:55:47 +0600

    Update mos-btt-settings.php

--- mos-btt-settings.php
+++ mos-btt-settings.php
@@ -138,6 +138,15 @@ function mos_btt_field_btt_padding_cb( $args ) {
 	<input name="mos_btt_options[<?php echo esc_attr( $args['label_for'] ); ?>][left]" id="<?php echo esc_attr( $args['label_for'] ); ?>" class="small-text" value="<?php echo isset( $options[ $args['label_for'] ]['left'] ) ? esc_html_e($options[$args['label_for']]['left']) : '';?>" placeholder="Left">
 	<?php
 }
+
+function mos_btt_field_btt_border_cb( $args ) {
+	$options = get_option( 'mos_btt_options' );
+	?>
+	<input name="mos_btt_options[<?php echo esc_attr( $args['label_for'] ); ?>]" id="<?php echo esc_attr( $args['label_for'] ); ?>" type='text' class='regular-text' value="<?php echo isset( $options[ $args['label_for'] ] ) ? esc_html_e($options[$args['label_for']]) : $args['default'];?>" required>
+	<small class="form-text text-muted">1px solid #000000</small>
+
+	<?php
+}
 function mos_btt_field_btt_position_cb( $args ) {
 	$options = get_option( 'mos_btt_options' );
 	?>
